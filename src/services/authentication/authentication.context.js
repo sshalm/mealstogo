84a982986f66031ceb600cc543c644909274de4e
sshalm <8477.shal@gmail.com>
2023-07-01 20:20:15 +1000

    Initial Entry

---
+++ src/services/authentication/authentication.context.js
@@ -0,0 +1,47 @@
+import React, { useState, createContext } from "react";
+import * as firebase from "firebase";
+
+import { loginRequest } from "./authentication.service";
+
+export const AuthenticationContext = createContext();
+
+export const AuthenticationContextProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  const onLogin = (email, password) => {
+    setIsLoading(true);
+    setUser({})
+  };
+
+  const onRegister = (email, password, repeatedPassword) => {
+    setIsLoading(true);
+    if (password !== repeatedPassword) {
+      setError("Error: Passwords do not match");
+      return;
+    }
+    setUser({})
+  };
+
+  const onLogout = () => {
+   setUser(null);
+        setError(null);
+  };
+
+  return (
+    <AuthenticationContext.Provider
+      value={{
+        isAuthenticated: !!user,
+        user,
+        isLoading,
+        error,
+        onLogin,
+        onRegister,
+        onLogout,
+      }}
+    >
+      {children}
+    </AuthenticationContext.Provider>
+  );
+};
